7fc7735b979d43cf6fd4fe738fc90bfe82ab12b2
*Luan Calazans* <138558778+Luanqmata@users.noreply.github.com>
2023-09-11 16:37:02 -0300

    Dale Dele dayle Dole

--- L3°- Divisiveis_5_While.py
+++ L3°- Divisiveis_5_While.py
@@ -8,7 +8,7 @@ def divisiveis_5():
         conta = contador / 5
         print(contador, '/  5  =', conta)
         print('\nprocessando...\n')
-        time.sleep(1)
+        time.sleep(1) ## ISSO NAO É NESCESSARIO É SO PARA STYLE.
         contador -= 5
         
 divisiveis_5()    
