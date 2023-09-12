bf103a09f750a8940191f24b1a738b26f29121dc
*Luan Calazans* <138558778+Luanqmata@users.noreply.github.com>
2023-09-12 01:43:04 -0300

    Update and rename errado.py to L3°-Jogo_adivinha_aprimorado.py

--- L3°-Jogo_adivinha_aprimorado.py
+++ L3°-Jogo_adivinha_aprimorado.py
@@ -9,10 +9,13 @@ num1 = jogo_adivinha()
 contador = 0
 
 while True:
-    numero_escolhido = int(input("Digite sair para sair.\nDigite o numero do jogo adivinha: '0 a 10'. "))
-    num2 = numero_escolhido
+    numero_escolhido = input("Digite sair para sair.\nDigite o numero do jogo adivinha: '0 a 10'. ")
+    
+    if numero_escolhido == 'sair':
+        exit()
+    num2 = int(numero_escolhido)
     if num1 == num2:
-        print(f"parabens você acertou.\nE você tentou{contador} vezes.")
+        print(f"parabens você acertou.\nE você tentou {contador} vezes.")
         break
     elif num1>num2:
         print("Numero do maquina é MAIOR.")
@@ -32,5 +35,3 @@ while True:
         print('...')
         time.sleep(0.5)
         contador += 1
-    elif numero_escolhido == 'sair':
-        break
