bdee3f53762e2583bf578ca599f2b72cdd4222f5
*Luan Calazans* <138558778+Luanqmata@users.noreply.github.com>
2023-09-12 02:34:00 -0300

    Update L3°- 2000_3200_%7_Não_%5.py

--- L3°- 2000_3200_%7_Não_%5.py
+++ L3°- 2000_3200_%7_Não_%5.py
@@ -4,7 +4,7 @@ def contador_2000a_3200():
     contador = 2000
     contador_7= -16
     contador_5= -92
-    while contador <= 3200:
+    while contador <= 3201:
         if contador % 7 == 0:
             print('Divisiveis por 7: ',contador) ## SE NAO QUISER MOSTRAR A TABELA É SO APAGAR ESTA LINHA DE CODIGO 
             contador_7 +=1
