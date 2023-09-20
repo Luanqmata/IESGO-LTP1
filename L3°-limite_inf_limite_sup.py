f4c00260a505569eb716230f730c1c1a9afe9817
*Luan Calazans* <138558778+Luanqmata@users.noreply.github.com>
2023-09-20 17:51:06 -0300

    Exercicio2

--- L3°-limite_inf_limite_sup.py
+++ L3°-limite_inf_limite_sup.py
@@ -1,5 +1,8 @@
-# Exerício 2 - Escreva um programa que solicita dois números inteirtos (sendo o primeiro o limite inferior e o segundo o limite superior) 
-# e exibe todos os números pares entre esses limites.
+# Exerício 2 - Escreva um programa que solicita dois números inteirtos (sendo o primeiro o limite inferior e o segundo o limite superior)  
+
+# e exibe todos os números pares entre esses limites.                                     ##           https://github.com/Luanqmata/lixo/blob/main/TABUADA_WHILE_FOR.py             (TEORIA)
+
+
 
 print('----------------------------------\nEm for.\n')
 começo = 5
