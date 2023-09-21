00a926b40c51c9ca249875aed3f467b7da14e505
*Luan Calazans* <138558778+Luanqmata@users.noreply.github.com>
2023-09-21 01:07:55 -0300

    Update L3°-Fatorial_Qlqr_N.py

--- L3°-Fatorial_Qlqr_N.py
+++ L3°-Fatorial_Qlqr_N.py
@@ -1,7 +1,7 @@
 # Exercício 6 - Escreva um aplicativo que solicita ao usuário inserir um número e retorna o seu fatorial.
 def fatorial(numero):
     resultado = 1
-    for i in range(1,numero+1): ## O LOOPING VAO DO 1 ',' ATE O NUMERO ESCOLHO 'NUMERO' CADA VEZ ACRESENTANDO '+1' 
+    for i in range(1,numero+1): ## O LOOPING VAO DO 1 ',' ATE O NUMERO ESCOLHIDO 'NUMERO' CADA VEZ ACRESENTANDO '+1' 
         resultado *= i ## aqui é o comando que Você da para o loop se vc qr ,multi,somar,dividir.   
     return resultado
 
