24bea22b53b191cca9399ab9c0f6a69d00d1839e
*Luan Calazans* <138558778+Luanqmata@users.noreply.github.com>
2023-09-22 13:04:43 -0300

    Update L3°- Divisiveis_5_While.py

--- L3°- Divisiveis_5_While.py
+++ L3°- Divisiveis_5_While.py
@@ -4,7 +4,7 @@ def divisiveis_5():
     print('numeros de 1 a 100 divisiveis por 5.')
     contador = 100
     print('\n Nao sei usar for \n') 
-    while contador >= 10:
+    while contador >= 5:
         conta = contador / 5
         print(contador, '/  5  =', conta)
         print('\nprocessando...\n')
