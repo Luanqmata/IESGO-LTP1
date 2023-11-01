4e2c2339b7a98358e2d072544872ca5ac6e717ac
*Luan Calazans* <138558778+Luanqmata@users.noreply.github.com>
2023-11-01 00:43:46 -0300

    Não aguento mais

--- Unidade B /zoo.py
+++ Unidade B /zoo.py
@@ -1,15 +1,5 @@
-"""
-
-Desafio:
-
-Adicione um atributo habitat à classe Animal (por exemplo, "Savana", "Tundra", "Floresta Tropical"). Modifique o método descricao para incluir essa informação.
-Na classe Zoologico, adicione um método buscar_por_especie(self, especie) que retorna uma lista de animais dessa espécie específica.
-Implemente a possibilidade de listar todos os animais em um habitat específico.
-Dicas:
-
-Ao listar os animais, use um loop para iterar sobre todos os animais e chamar o método descricao de cada animal.
-"""
 import time
+
 class Animal:
     def __init__(self, nome, especie, idade, dieta, habitat):
         self.nome = nome
@@ -79,15 +69,14 @@ class Zoo:
             print('\nTodos Animais Que já passaram Pelo O ZOO LOGIC:')
             print('\n\t\t',self.lista_animais,'\n\n\n\n\n')
     
-    #def listar_animais_em_habitat(self, habitat):
-      #  print(f'\nAnimais no habitat {habitat}:')
-       # animais_no_habitat = [animal for animal in self.lista_animais if animal.habitat == habitat]
-      #  for animal in animais_no_habitat:
-      #      print(f'{animal.nome.title()} ({animal.especie})')
-      #  print('\n\n\n\n\n')
+    def listar_animais_em_habitat(self, habitat):
+        print(f'\nAnimais no habitat {habitat}:')
+        for animal in self.lista_animais:
+            if animal[4] == habitat.lower():
+                print(animal[0])
+        print('\n\n\n\n\n')
 
-        
-        
+
 zoo = Zoo() 
 time.sleep(0.2)    
 print('\n\t@@@@@@@@@@@@@@@@@@@@@@@@@ BEM-VINDO AO ZoO LoGIc @@@@@@@@@@@@@@@@@@@@@@@@@')
@@ -134,9 +123,9 @@ while True:
     elif user_escolha ==3:
         zoo.listar_animais()
     
-  #  elif user_escolha == 4:
-    #    habitat = input('Digite o habitat para listar os animais: ')
-    #    zoo.listar_animais_em_habitat(habitat.lower())
+    elif user_escolha == 4:
+        habitat = input('Digite o habitat para listar os animais: ')
+        zoo.listar_animais_em_habitat(habitat)
     
     else:
-        print('Opção inválida. Digite Os Números Disponiveis.')
+        print('Opção inválida. Digite 1 para registrar um animal ou 2 para sair.')
